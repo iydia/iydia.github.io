5de09067ae2b0f327f8acfaa99947603e4a9a534
Lydia He <75390984+iydia@users.noreply.github.com>
2024-01-21 00:08:57 -0500

    Animation

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ const myText2 = new SplitType('p', {charClass: 'char2'});
 var tl = gsap.timeline({defaults:{ease: "Expo.easeInOut"}});
 
 tl.from('.char', {
-    y: -100,
+    y: 0,
     stagger: 0.05,
-    delay: 1
+    delay: 0,
 })
 .from('.char2', {
-    y: -100,
+    y: -10,
     stagger: 0.05,
-    duration: 0.6
+    duration: 1
 }, "-=1.2")
 .to('h1,p', {
     y: 80,
