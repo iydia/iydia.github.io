ccac10b553b3c4d8da5297ad227a755988dd31e7
Lydia He <75390984+iydia@users.noreply.github.com>
2024-01-21 00:40:54 -0500

    Smoother

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-const headLine = new SplitType('h1');
-const myText2 = new SplitType('p', {charClass: 'char2'});
 var tl = gsap.timeline({defaults:{ease: "Expo.easeInOut"}});
 
 tl.from('.char', {
-    y: 0,
-    stagger: 0.05,
-    delay: 0,
-})
+    stagger: -0.05
+}, "-=1.2")
 .from('.char2', {
-    y: -10,
     stagger: 0.05,
-    duration: 1
+    duration: 0.5
 }, "-=1.2")
 .to('h1,p', {
     y: 80,
